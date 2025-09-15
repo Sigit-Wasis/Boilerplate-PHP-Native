4fa134bd0e69aebf522a1479d616766360381af3
setianingsihnew23-droid <setianingsihnew23gmail.com>
2025-09-15 12:10:30 +0700

    schema database

--- database/schema.sql
+++ database/schema.sql
@@ -86,4 +86,21 @@ CREATE TABLE media(
 
 );
 
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE permissions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE role_permissions (
+    role_id INT,
+    permission_id INT,
+    PRIMARY KEY (role_id, permission_id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (permission_id) REFERENCES permissions(id)
+);
 
